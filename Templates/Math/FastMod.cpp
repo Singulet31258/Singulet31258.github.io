a956dde67809824762c8f33330dd8724f8b3e179
Singulet31258 <119833992+Singulet31258@users.noreply.github.com>
2023-01-01 12:16:13 +0800

    Create FastMod.cpp

---
+++ Templates/Math/FastMod.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using i8=char;
+using i16=short;
+using i32=long;
+using i64=long long;
+using i128=__int128_t;
+using u8=unsigned char;
+using u16=unsigned short;
+using u32=unsigned long;
+using u64=unsigned long long;
+using u128=__uint128_t;
+constexpr u128 A=1,B=A<<64;
+/**
+ * 内部实现为Barrett Reduction的快速取模结构体
+ * 在对固定模数p取模1e9次的情况下：（ISO C++14 O2 GCC 9.3.0）
+ * 对变量p取模：4800~5000ms；
+ * 对常量p取模：640~680ms；
+ * 对只读变量p取模：4750~5000ms；
+ * 把p封装到该结构体后取模：720~750ms；
+ * 该结构体目前已经非常接近编译器对取模优化的效果，但仍有进一步的优化空间
+ * 用法：(const)fastmod m(p);（const可加可不加）然后n对p取模就可以写成n%m
+ * m在定义以后就无法修改，可以将m定义成constexpr
+ * m在定义时会做一次低效的整数除法运算，因此在模数频繁变更的情况下不要使用该结构体！
+*/
+struct fastmod{
+    const int a;
+    const u64 b,p;
+    constexpr fastmod(const u64&p)noexcept:a(64-__builtin_clzll(p-1)),b(((A<<a)|(B<<a))/p),p(p){}
+    constexpr friend u64 operator/(const u64&n,const fastmod&m)noexcept{
+        return n+u64(u128(n)*m.b>>64)>>m.a;
+    }
+    constexpr friend u64 operator%(const u64&n,const fastmod&m)noexcept{
+        return n-(n+u64(u128(n)*m.b>>64)>>m.a)*m.p;
+    }
+};
+constexpr fastmod M(1e9+7);
+u64 n,p;
+int main(){
+    using namespace std;
+    cin>>n>>p;
+    const fastmod m(p);
+    cout<<n%p<<' '<<n%m<<endl;
+    return 0;
+}
